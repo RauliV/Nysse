56beb4d1d77f8de1875113638a541cc5f1fb2bac
Rauli Virtanen <rauli.virtanen@tuni.fi>
2020-11-17 17:29:21 +0200

    Siivottu koodia. Kommentteina kuvatut ongelmat siirretty Trelloon

--- Game/main.cc
+++ Game/main.cc
@@ -18,8 +18,9 @@ int main(int argc, char *argv[])
 
     std::shared_ptr<Interface::ICity> pointer;
 
-    CourseSide::SimpleMainWindow* window = new CourseSide::SimpleMainWindow[1];
-    window->show();
+    CourseSide::SimpleMainWindow window;
+   //CourseSide::SimpleMainWindow* window = new CourseSide::SimpleMainWindow[1];
+    window.show();
 
     QString file_iso = ":/offlinedata/offlinedata/kartta_iso_1095x592.png";
     QString file_pieni = ":/offlinedata/offlinedata/kartta_pieni_500x500.png";
@@ -27,43 +28,22 @@ int main(int argc, char *argv[])
     QImage tausta_pieni (file_pieni);
 
 
+
     //pointer = Interface::createGame(); //   TÄSSÄ ROPLEEMI.
+
     // Miksi createGame ei ole käytettävissä täällä? Ajonaikainen linkitysvirhe?
     // Sama tiedostossa city.cc 94
-
-
 
     //Jumittavat, koska pointteri viittaa jonnekin(tm). Kts. edellä.
     //pointer->setBackground(tausta_iso, tausta_pieni);
     //pointer->setClock(QTime::currentTime());
     //pointer->startGame(); //slot
 
-    // pelimoottori
-
-    // Game Over
-
-    // statistics -> save stats
-
-
     return a.exec();
 
+
 }
-/*
-namespace name {
-
-//std::shared_ptr<CourseSide::SimpleMainWindow> ikkuna_osoite = new (CourseSide::SimpleMainWindow);
-CourseSide::SimpleMainWindow *ikkuna_osoite =  new CourseSide::SimpleMainWindow;
 
 
-}*/
-
-
-// Kommentit/kysymykset jne...
-
-// Miten ikkunaolio näkyviin missä tarvitaan / globaaliksi vai tarvitaanko muualla kuin mainissa.
-// (Esim. City::setBackground city.cc 45)
-
-// Toimijatyypit (int)? IActorista periytetty valmiiksi IVechile ja IPassenger.
-// Valitaanko tyyppinumerot itse vai hä?
 
 
