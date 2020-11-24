7af3e983a1d54686b8a89fcdf2b95ecfe5fa3a02
Rauli Virtanen <rauli.virtanen@tuni.fi>
2020-11-24 19:40:46 +0200

    Portrait kuvia muokattu, MainWindow nyt oikean kokoinen - staattinen. Scrollbarit näkyvissä.

--- Game/game_engine.cpp
+++ Game/game_engine.cpp
@@ -1,5 +1,5 @@
 #include "core/logic.hh"
-#include "game_engine.h"
+//#include "game_engine.h"
 
 
 
@@ -11,10 +11,12 @@
 //onko liikkunut (QTime x QTime x1 loc !)
 //päivitä
 //saapuu kohteeseen ->
-
-void make_game(std::shared_ptr<Interface::ICity> city);
 /*
+void make_game(std::shared_ptr<Interface::ICity> city);
+
 std::shared_ptr<CourseSide::Logic> Game = std::make_shared<CourseSide::Logic> ();
 Game->takeCity(city);
+Game->finalizeGameStart();
+
 
 };*/
